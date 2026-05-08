09052cfb7a6bb1f399fcbb8efba8ef2d26a272ea
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2026-05-08 11:00:57 +0300

    Update job time allocation in run_baseline_experiment.sh to 36 hours

--- run_baseline_experiment.sh
+++ run_baseline_experiment.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:v100:1
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 #SBATCH --output=slurm-%j-baselines.out
  
 cd /scratch/project_2019043/ms-thesis
